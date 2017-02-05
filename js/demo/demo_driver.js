ad1f030e5621e79d9e40a84fc2d770d4299c612e
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2017-02-04 23:31:12 -0800

    simple force spring layout

--- js/demo/demo_driver.js
+++ js/demo/demo_driver.js
@@ -56,8 +56,89 @@
             return "/traffic_status";
         });
 
+	function Graph(){
+		this.initialize = function(){
+			this.svg = d3.select("svg");
+			this.width = +this.svg.attr("width");
+			this.height = +this.svg.attr("height");
+
+			this.color = d3.scaleOrdinal(d3.schemeCategory20);
+
+			this.simulation = d3.forceSimulation()
+				.force("link", d3.forceLink().id(function(d) { return d.id; }))
+				.force("charge", d3.forceManyBody())
+				.force("center", d3.forceCenter(this.width/2, this.height/2));
+		};
+		this.addData = function(graph){
+			var link = this.svg.append("g")
+				.attr("class", "links")
+				.selectAll("line")
+				.data(graph.links)
+				.enter().append("line")
+				  .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+			var color = this.color;
+			var node = this.svg.append("g")
+				  .attr("class", "nodes")
+				.selectAll("circle")
+				.data(graph.nodes)
+				.enter().append("circle")
+				  .attr("r", 5)
+				  .attr("fill", function(d) { return color(d.group); })
+				  .call(d3.drag()
+					  .on("start", this.dragstarted)
+					  .on("drag", this.dragged)
+					  .on("end", this.dragended));
+
+			  node.append("title")
+				  .text(function(d) { return d.id; });
+
+			this.simulation
+				  .nodes(graph.nodes)
+				  .on("tick", ticked);
+
+			this.simulation.force("link")
+				.links(graph.links);
+
+			function ticked() {
+				link
+					.attr("x1", function(d) { return d.source.x; })
+					.attr("y1", function(d) { return d.source.y; })
+					.attr("x2", function(d) { return d.target.x; })
+					.attr("y2", function(d) { return d.target.y; });
+
+				node
+					.attr("cx", function(d) { return d.x; })
+					.attr("cy", function(d) { return d.y; });
+			}
+		};
+
+		this.dragstarted = function (d) {
+		  if (!d3.event.active) this.simulation.alphaTarget(0.3).restart();
+		  d.fx = d.x;
+		  d.fy = d.y;
+		}
+
+		this.dragged = function (d) {
+		  d.fx = d3.event.x;
+		  d.fy = d3.event.y;
+		}
+
+		this.dragended = function (d) {
+		  if (!d3.event.active) this.simulation.alphaTarget(0);
+		  d.fx = null;
+		  d.fy = null;
+		}
+	};
+
+	graph = new Graph();
+	graph.initialize();
+
     $(df2).on({
         "stateFetchingSuccess": function(event, data) {
+			d3.json("miserables.json", function(error, graph2) {
+				graph.addData(graph2);
+			});
             data.result.data.forEach(function(dataEntry) {
                 addNewEntry($trafficStatusList, JSON.stringify(dataEntry));
             });
